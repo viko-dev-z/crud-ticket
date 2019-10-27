86864b9711a89bdb2ff2b2dc6ca27f80ff81ffd2
viko-dev-z <viko.dev.z@gmail.com>
2019-10-27 18:50:35 -0400

    second commit - Setting up spring project and most important gradle dependencies

---
+++ src/main/java/com/example/crudticket/CrudTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudticket/CrudTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
